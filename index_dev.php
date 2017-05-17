722938502f67f65e880be4eb519a102015b2b0e1
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-05-17 19:53:02 -0400

    index for dev environment

---
+++ index_dev.php
@@ -0,0 +1,12 @@
+<?php
+
+ini_set('display_errors', 0);
+
+$loader = require_once __DIR__.'/vendor/autoload.php';
+
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
+$app = require __DIR__.'/src/app.php';
+require __DIR__.'/config/dev.php';
+
+$app->run();
