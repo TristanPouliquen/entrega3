631714eda165ea23cbba8655fbb57a0e8a654e27
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-05-18 15:22:28 -0400

    Add bootstrap file

---
+++ src/bootstrap.php
@@ -0,0 +1,54 @@
+<?php
+
+use Dflydev\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\TwigServiceProvider());
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), [
+    'dbs.options' => [
+        'grupo37' => [
+            'driver'    => 'pdo_pgsql',
+            'host'      => 'localhost',
+            'dbname'    => 'grupo37',
+            'user'      => 'grupo37',
+            'password'  => 'grupo37',
+        ],
+        'grupo40' => [
+            'driver'    => 'pdo_pgsql',
+            'host'      => 'localhost',
+            'dbname'    => 'grupo40',
+            'user'      => 'grupo40',
+            'password'  => 'grupo40',
+        ],
+    ],
+]);
+
+$app->register(new DoctrineOrmServiceProvider, [
+    'orm.ems.options' => [
+        'grupo37' => [
+            'connection' => 'grupo37',
+            'mappings' => [
+                [
+                    'type' => 'yml',
+                    'namespace' => 'Entity37',
+                    'path' => __DIR__ . '/../config/doctrine/grupo37'
+                ]
+            ]
+        ],
+        'grupo40' => [
+            'connection' => 'grupo40',
+            'mappings' => [
+                [
+                    'type' => 'yml',
+                    'namespace' => 'Entity40',
+                    'path' => __DIR__ . '/../config/doctrine/grupo40'
+                ]
+            ]
+        ]
+    ],
+    'orm.proxies_dir' => __DIR__.'/../var/cache/doctrine/proxies'
+]);
+
+return $app;
