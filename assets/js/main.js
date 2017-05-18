6665e017cc750a0f81a1daa53cec9df8aaebf346
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-05-18 16:13:41 -0400

    js animation speed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 $(window).load(function() { // makes sure the whole site is loaded
       $('#status').fadeOut(); // will first fade out the loading animation
-      $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-      $('body').delay(350).css({'overflow':'visible'});
+      $('#preloader').delay(200).fadeOut('slow'); // will fade out the white DIV that covers the website.
+      $('body').delay(200).css({'overflow':'visible'});
     })
 
 $(document).ready(function() {
- 
+
   $("#bg-slider").owlCarousel({
       navigation : false, // Show next and prev buttons
       slideSpeed : 100,
@@ -14,13 +14,13 @@ $(document).ready(function() {
       singleItem:true,
       mouseDrag: false,
       transitionStyle : "fade"
- 
+
       // "singleItem:true" is a shortcut for:
-      // items : 1, 
+      // items : 1,
       // itemsDesktop : false,
       // itemsDesktopSmall : false,
       // itemsTablet: false,
-      // itemsMobile : false 
+      // itemsMobile : false
   });
 
   $("#testimonial-slider").owlCarousel({
@@ -31,13 +31,13 @@ $(document).ready(function() {
       singleItem:true,
       mouseDrag: false,
       transitionStyle : "goDown"
- 
+
       // "singleItem:true" is a shortcut for:
-      // items : 1, 
+      // items : 1,
       // itemsDesktop : false,
       // itemsDesktopSmall : false,
       // itemsTablet: false,
-      // itemsMobile : false 
+      // itemsMobile : false
   });
 
     $('.more-jobs a').click(function(e){
@@ -45,7 +45,7 @@ $(document).ready(function() {
       var $this = $(this);
       $this.toggleClass('more-jobs a');
       if($this.hasClass('more-jobs a')){
-        $this.text('View less jobs');     
+        $this.text('View less jobs');
       } else {
         $this.text('View more jobs');
       }
@@ -56,7 +56,7 @@ $(document).ready(function() {
     });
 
 
- 
+
 })
 
 // Initializing WOW.JS
