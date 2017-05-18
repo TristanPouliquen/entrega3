873260a9c1140b2cf871fbc7034c8d2f963f9aaf
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-05-18 00:46:40 -0400

    Hum... changed to PHP concatenation operator

--- src/routes.php
+++ src/routes.php
@@ -6,5 +6,5 @@ use Symfony\Component\HttpFoundation\Response;
 $app->get('/', function () use($app) {
     $hotel = $app["orm.ems"]["grupo40"]->getRepository("Entity40\Hotel")->findAll()[0];
     $restaurant = $app["orm.ems"]["grupo37"]->getRepository("Entity37\Restaurant")->findAll()[0];
-    return new Response($hotel->getName() + " - " + $restaurant->getName());
+    return new Response($hotel->getName() . " - " . $restaurant->getName());
 });
