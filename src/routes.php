59dd3ebcd45f475bea67e3f5d4080edaae4aee4e
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-05-17 23:58:36 -0400

    lighter display

--- src/routes.php
+++ src/routes.php
@@ -2,5 +2,5 @@
 
 $app->get('/', function () use($app) {
     $hotel = $app["orm.ems"]["grupo40"]->getRepository("Entity40\Hotel")->findAll()[0];
-    return new Response(json_encode($hotel));
+    return new Response(json_encode($hotel->getName()));
 });
