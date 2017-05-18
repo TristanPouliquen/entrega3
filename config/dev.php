c84a4c9eaa4fccff9a1a8f3aad30b428b7346d3c
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-05-18 14:22:25 -0400

    Useless var_dump

--- config/dev.php
+++ config/dev.php
@@ -5,9 +5,6 @@ use Silex\Provider\MonologServiceProvider;
 // include the prod configuration
 require __DIR__.'/prod.php';
 
-
-var_dump($app);die;
-
 // enable the debug mode
 $app['debug'] = true;
 
