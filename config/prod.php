fd2cdfce7432949b3e046c4a96c76d17a8235a1a
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-05-18 15:14:27 -0400

    Debug twig template loading

--- config/prod.php
+++ config/prod.php
@@ -1,8 +1,9 @@
 <?php
 
 // configure your app for the production environment
-$app['twig.path'] = array(__DIR__.'/../src/views');
+$app['twig.path'] = array(realpath(__DIR__.'/../src/views'));
 $app['twig.options'] = array(
-  'cache' => __DIR__.'/../var/cache/twig_cache',
+  'cache' => __DIR__.'/../var/cache/twig',
   'auto_reload' => true
 );
+var_dump($app['twig.path']);die;
