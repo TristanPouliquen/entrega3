9f4ed71d32a5e5ce5f18c465893dd98dc5df2594
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-05-18 00:33:27 -0400

    Add restaurant name

--- src/routes.php
+++ src/routes.php
@@ -5,5 +5,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 $app->get('/', function () use($app) {
     $hotel = $app["orm.ems"]["grupo40"]->getRepository("Entity40\Hotel")->findAll()[0];
-    return new Response($hotel->getName());
+    $restaurant = $app["orm.ems"]["grupo37"]->getRepository("Entity37\Restaurant")->findAll[0];
+    return new Response($hotel->getName() + " - " + $restaurant->getName());
 });
