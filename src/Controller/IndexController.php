08c7000ae94a6858c90aae0cac547062f17bbe60
diiru <diiru@uc.cl>
2017-05-21 17:32:42 -0400

    Ciudades

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -40,8 +40,9 @@ class IndexController implements ControllerProviderInterface {
         $cities40 = $em40->getRepository('Entity40\Address')->getDistinctCities();
         $em37 = $app['orm.ems']['grupo37'];
         $cities37 = $em40->getRepository('Entity37\Restaurant')->getDistinctCities();
+        $merge = array_unique(array_merge($cities37,$cities40))
         return $app['twig']->render('root/index.html.twig', [
-            'cities'=> $cities40
+            'cities'=> $merge
         ]);
     }
 
