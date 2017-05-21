cbd544f19edba73016bf31f479bc6e0f6a7ee9b9
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-05-20 21:03:53 -0400

    Missing semicolon

--- src/Lib/UserProvider.php
+++ src/Lib/UserProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Lib\UserProvider
+namespace Lib\UserProvider;
 
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
