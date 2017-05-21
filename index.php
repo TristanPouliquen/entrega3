87284ac294b57ae10cd54df55a0d4692fed005b9
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-05-21 14:55:10 -0400

    Dev by default

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $loader = require_once __DIR__.'/vendor/autoload.php';
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 $app = require __DIR__.'/src/bootstrap.php';
-require __DIR__.'/config/prod.php';
+require __DIR__.'/config/dev.php';
 require __DIR__.'/src/app.php';
 
 $app->run();
