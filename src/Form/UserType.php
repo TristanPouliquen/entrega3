75c56e69e081dd132c1a11a2ea4783c325d54b1c
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-05-25 23:49:30 -0400

    Name

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -16,7 +16,7 @@ class UserType extends AbstractType
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
     $builder
-      ->add('email', EmailType::class)
+      ->add('username', EmailType::class)
       ->add('plainPassword', PasswordType::class)
     ;
   }
