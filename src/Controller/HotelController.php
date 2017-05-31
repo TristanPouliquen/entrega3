a368d2196dced8a8449f2f13f80e7cb180cab625
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-05-30 22:03:45 -0400

    Reserve restaurant with hotel

--- src/Controller/HotelController.php
+++ src/Controller/HotelController.php
@@ -16,9 +16,15 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 
 use Entity40\Guest;
 use Entity40\Reservation;
+
+use Entity37\Reservation;
+use Entity37\ReservationRestaurant;
+use Entity37\Client;
 
 
 /**
@@ -45,140 +51,231 @@ class HotelController implements ControllerProviderInterface {
     }
 
     public function showAll(Application $app, Request $request) {
-        $em = $app['orm.ems']['grupo40'];
-        $form = $app['form.factory']->createBuilder(FormType::class, [])
-            ->add("name", TextType::class, [
-                'required' => false
-            ])
-            ->add("city", ChoiceType::class, [
-                'choices' => array_map(function($item){ return $item["city"];}, $em->getRepository("Entity40\Address")->getDistinctCities()),
-                'choice_label' => function($value, $key, $index) {
-                    return $value;
-                },
-                'placeholder' => "Elige tu ciudad",
-                "required" => false
-            ])
-            ->add("rating", ChoiceType::class, [
-                'choices' => [0,1,2,3,4,5],
-                'placeholder' => 'Minimum de estrellas',
-                "required" => false
-            ])
-            ->add("sort", ChoiceType::class, [
-                'choices' => ['Por ciudad' => 'ciudad', 'Alfabetico' => 'alfabetico', 'Por estrellas' => 'estrellas'],
-                'placeholder' => 'Ordenar los resultados...',
-                'required' => false
-            ])
-            ->getForm();
+      $em = $app['orm.ems']['grupo40'];
+      $form = $app['form.factory']->createBuilder(FormType::class, [])
+        ->add("name", TextType::class, [
+            'required' => false
+        ])
+        ->add("city", ChoiceType::class, [
+            'choices' => array_map(function($item){ return $item["city"];}, $em->getRepository("Entity40\Address")->getDistinctCities()),
+            'choice_label' => function($value, $key, $index) {
+                return $value;
+            },
+            'placeholder' => "Elige tu ciudad",
+            "required" => false
+        ])
+        ->add("rating", ChoiceType::class, [
+            'choices' => [0,1,2,3,4,5],
+            'placeholder' => 'Minimum de estrellas',
+            "required" => false
+        ])
+        ->add("sort", ChoiceType::class, [
+            'choices' => ['Por ciudad' => 'ciudad', 'Alfabetico' => 'alfabetico', 'Por estrellas' => 'estrellas'],
+            'placeholder' => 'Ordenar los resultados...',
+            'required' => false
+        ])
+        ->getForm();
 
-        $form->handleRequest($request);
+      $form->handleRequest($request);
 
-        if ($form->isSubmitted()){
-            $data = $form->getData();
-            $hotels = $em->getRepository('Entity40\Hotel')->getFiltered($data);
+      if ($form->isSubmitted()){
+        $data = $form->getData();
+        $hotels = $em->getRepository('Entity40\Hotel')->getFiltered($data);
 
-        } else {
-            $hotels = $em->getRepository('Entity40\Hotel')->findAll();
-        }
+      } else {
+        $hotels = $em->getRepository('Entity40\Hotel')->findAll();
+      }
 
-        return $app['twig']->render('hotel/list.html.twig', [
-            'hotels' => $hotels,
-            'form' => $form->createView()
-        ]);
+      return $app['twig']->render('hotel/list.html.twig', [
+        'hotels' => $hotels,
+        'form' => $form->createView()
+      ]);
     }
 
     public function detail(Application $app, $id){
-        $em40 = $app['orm.ems']['grupo40'];
-        $em37 = $app['orm.ems']['grupo37'];
-        $hotel = $em40->getRepository('Entity40\Hotel')->find($id);
-        if(!$hotel){
-            $app['session']->getFlashBag()->add('error', 'Hotel no encontrado');
-            return $app->redirect($app['url_generator']->generate('hotel_list'));
-        }
-        $restaurant = $em37->getRepository('Entity37\Restaurant')->findOneBy([
-            'city' => $hotel->getAddress()->getCity(),
-            'street' => $hotel->getAddress()->getNumber() . ' ' . $hotel->getAddress()->getStreet()
-        ]);
-        return $app['twig']->render('hotel/detail.html.twig', [
-            'hotel' => $hotel,
-            'restaurant' => $restaurant
-        ]);
+      $em40 = $app['orm.ems']['grupo40'];
+      $em37 = $app['orm.ems']['grupo37'];
+      $hotel = $em40->getRepository('Entity40\Hotel')->find($id);
+      if(!$hotel){
+        $app['session']->getFlashBag()->add('error', 'Hotel no encontrado');
+        return $app->redirect($app['url_generator']->generate('hotel_list'));
+      }
+      $restaurant = $em37->getRepository('Entity37\Restaurant')->findOneBy([
+        'city' => $hotel->getAddress()->getCity(),
+        'street' => $hotel->getAddress()->getNumber() . ' ' . $hotel->getAddress()->getStreet()
+      ]);
+      return $app['twig']->render('hotel/detail.html.twig', [
+        'hotel' => $hotel,
+        'restaurant' => $restaurant
+      ]);
     }
 
     public function reserve(Application $app, Request $request, $id){
-        $em = $app['orm.ems']['grupo40'];
-        $hotel = $em->getRepository('Entity40\Hotel')->find($id);
-        if(!$hotel){
-            $app['session']->getFlashBag()->add('error', 'Hotel no encontrado');
-            return $app->redirect($app['url_generator']->generate('hotel_list'));
+      $em = $app['orm.ems']['grupo40'];
+      $em37 = $app['orm.ems']['grupo37'];
+      $hotel = $em->getRepository('Entity40\Hotel')->find($id);
+      if(!$hotel){
+        $app['session']->getFlashBag()->add('error', 'Hotel no encontrado');
+        return $app->redirect($app['url_generator']->generate('hotel_list'));
+      }
+      $restaurant = $em37->getRepository('Entity37\Restaurant')->findOneBy([
+        'city' => $hotel->getAddress()->getCity(),
+        'street' => $hotel->getAddress()->getNumber() . ' ' . $hotel->getAddress()->getStreet()
+      ]);
+
+      $formBuilder = $app['form.factory']->createBuilder(FormType::class, [])
+        // Guest info
+        ->add("name")
+        ->add('identityNumber')
+        ->add("phoneNumber")
+        ->add('birthdate', DateType::class, [
+            'widget' => 'single_text'
+        ])
+        // Reservation info
+        ->add("arrival", DateType::class, [
+            'widget' => 'single_text'
+        ])
+        ->add('duration', IntegerType::class)
+        ->add('paymentMethod', ChoiceType::class, [
+            'choices' => ['Efectivo', 'Tarjeta de credito', 'Tarjeta de debito', 'Transferencia'],
+            'choice_label' => function($value, $key, $index) {
+                return $value;
+            },
+            'placeholder' => 'Elige tu metodo de pago'
+        ])
+        ->add("submit", SubmitType::class);
+
+      if ($restaurant){
+        $formBuilder->add('reserve_lunches', CheckBoxType::class)
+          ->add('hour_lunches', TimeType::class, [
+            'widget' => 'choice',
+            'minutes' => ['00'],
+            'required' => false
+          ])
+          ->add('quantity_lunches', IntegerType::class[
+            'attr' => [
+              'min' => 0,
+              'max' => $restaurant->getCapacity()
+            ],
+            'required' => false
+          ])
+          ->add('reserve_dinners', CheckBoxType::class)
+          ->add('hour_dinners', TimeType::class, [
+            'widget' => 'choice',
+            'minutes' => ['00'],
+            'required' => false
+          ])
+          ->add('quantity_dinners', IntegerType::class, [
+            'attr' => [
+              'min' => 0,
+              'max' => $restaurant->getCapacity()
+            ],
+            'required' => false
+          ]);
+      }
+
+      $form = $formBuilder->getForm();
+
+      $form->handleRequest($request);
+
+      if ($form->isSubmitted()){
+        $data = $form->getData();
+        $guest = $em->getRepository('Entity40\Guest')->findOneBy(['identity_number' => $data['identityNumber']]);
+        if(!$guest){
+          $guest = new Guest();
+          $guest->setName($data['name']);
+          $guest->setIdentityNumber($data['identityNumber']);
+          $guest->setPhoneNumber($data['phoneNumber']);
+          $guest->setBirthdate($data['birthdate']);
         }
 
-        $form = $app['form.factory']->createBuilder(FormType::class, [])
-            // Guest info
-            ->add("name")
-            ->add('identityNumber')
-            ->add("phoneNumber")
-            ->add('birthdate', DateType::class, [
-                'widget' => 'single_text'
-            ])
-            // Reservation info
-            ->add("arrival", DateType::class, [
-                'widget' => 'single_text'
-            ])
-            ->add('duration', IntegerType::class)
-            ->add('paymentMethod', ChoiceType::class, [
-                'choices' => ['Efectivo', 'Tarjeta de credito', 'Tarjeta de debito', 'Transferencia'],
-                'choice_label' => function($value, $key, $index) {
-                    return $value;
-                },
-                'placeholder' => 'Elige tu metodo de pago'
-            ])
-            ->add("submit", SubmitType::class)
-            ->getForm();
+        $reservation = new Reservation();
+        $reservation->setArrival($data['arrival']);
+        $reservation->setDuration($data['duration']);
+        $reservation->setPaymentMethod($data['paymentMethod']);
+        $reservation->setHotel($hotel);
+        $reservation->setGuest($guest);
 
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted()){
-            $data = $form->getData();
-            $guest = $em->getRepository('Entity40\Guest')->findOneBy(['identity_number' => $data['identityNumber']]);
-            if(!$guest){
-                $guest = new Guest();
-                $guest->setName($data['name']);
-                $guest->setIdentityNumber($data['identityNumber']);
-                $guest->setPhoneNumber($data['phoneNumber']);
-                $guest->setBirthdate($data['birthdate']);
+        if ($data['reserve_dinners'] && $data['hour_dinners'] && $data['quantity_dinners']) {
+          for ($i = 0; $i < $reservation->getDuration(); i++){
+            $client_dinner = $em3737->getRepository('Entity37\Client')->findOneBy(['phone_number' => $data['phoneNumber']]);
+            if (!$client_dinner){
+              $client_dinner = new Client();
+              $client_dinner->setPhoneNumber($data['phoneNumber']);
+              $client_dinner->setName($data["name"]);
             }
 
-            $reservation = new Reservation();
-            $reservation->setArrival($data['arrival']);
-            $reservation->setDuration($data['duration']);
-            $reservation->setPaymentMethod($data['paymentMethod']);
-            $reservation->setHotel($hotel);
-            $reservation->setGuest($guest);
+            $reservation_dinner = new Reservation();
+            $fecha = date_create($data['arrival']);
+            date_add($fecha, date_interval_create_from_date_string($i . " days"));
+            $reservation_dinner->setDate($fecha);
+            $reservation_dinner->setTime($data['hour_dinners']);
+            $reservation_dinner->setQuantity($data['quantity_dinners']);
 
-            $em->persist($guest);
-            $em->persist($reservation);
-            $em->flush();
+            $reservationRestaurant_dinner = new ReservationRestaurant();
+            $reservationRestaurant_dinner->setClient($client_dinner);
+            $reservationRestaurant_dinner->setRestaurant($restaurant);
+            $reservationRestaurant_dinner->setReservation($reservation_dinner);
 
-            $app['session']->getFlashBag()->add('success', 'Reserva hecha exitosamente');
-            return $app->redirect($app['url_generator']->generate('hotel_detail', ['id' => $id]));
+            $em37->persist($client_dinner);
+            $em37->persist($reservation_dinner);
+            $em37->persist($reservationRestaurant_dinner);
+            $em37->flush();
+          }
         }
 
-        return $app['twig']->render('hotel/reserve.html.twig', [
-            'hotel' => $hotel,
-            'form' => $form->createView()
-        ]);
+        if ($data['reserve_lunches'] && $data['hour_lunches'] && $data['quantity_lunches']) {
+          for ($i = 0; $i < $reservation->getDuration(); i++){
+            $client_lunch = $em37->getRepository('Entity37\Client')->findOneBy(['phone_number' => $data['phoneNumber']]);
+            if (!$client_lunch){
+              $client_lunch = new Client();
+              $client_lunch->setPhoneNumber($data['phoneNumber']);
+              $client_lunch->setName($data["name"]);
+            }
+
+            $reservation_lunch = new Reservation();
+            $fecha = date_create($data['arrival']);
+            date_add($fecha, date_interval_create_from_date_string($i . " days"));
+            $reservation_lunch->setDate($fecha);
+            $reservation_lunch->setTime($data['hour_lunches']);
+            $reservation_lunch->setQuantity($data['quantity_lunches']);
+
+            $reservationRestaurant_lunch = new ReservationRestaurant();
+            $reservationRestaurant_lunch->setClient($client_lunch);
+            $reservationRestaurant_lunch->setRestaurant($restaurant);
+            $reservationRestaurant_lunch->setReservation($reservation_lunch);
+
+            $em37->persist($client_lunch);
+            $em37->persist($reservation_lunch);
+            $em37->persist($reservationRestaurant_lunch);
+            $em37->flush();
+          }
+        }
+
+        $em->persist($guest);
+        $em->persist($reservation);
+        $em->flush();
+
+        $app['session']->getFlashBag()->add('success', 'Reserva hecha exitosamente');
+        return $app->redirect($app['url_generator']->generate('hotel_detail', ['id' => $id]));
+      }
+
+      return $app['twig']->render('hotel/reserve.html.twig', [
+        'hotel' => $hotel,
+        'form' => $form->createView()
+      ]);
     }
 
     public function detailReservations(Application $app, $id){
-        $em = $app['orm.ems']['grupo40'];
-        $hotel = $em->getRepository('Entity40\Hotel')->find($id);
-        if(!$hotel){
-            $app['session']->getFlashBag()->add('error', 'Hotel no encontrado');
-            return $app->redirect($app['url_generator']->generate('hotel_list'));
-        }
+      $em = $app['orm.ems']['grupo40'];
+      $hotel = $em->getRepository('Entity40\Hotel')->find($id);
+      if(!$hotel){
+        $app['session']->getFlashBag()->add('error', 'Hotel no encontrado');
+        return $app->redirect($app['url_generator']->generate('hotel_list'));
+      }
 
-        return $app['twig']->render('hotel/detail_reservations.html.twig', [
-            'hotel' => $hotel
-        ]);
+      return $app['twig']->render('hotel/detail_reservations.html.twig', [
+        'hotel' => $hotel
+      ]);
     }
 }
--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -29,156 +29,155 @@ use Entity37\Client;
  */
 class RestaurantController implements ControllerProviderInterface {
 
-    /**
-     * Route settings
-     *
-     */
-    public function connect(Application $app) {
-        $restaurantController = $app['controllers_factory'];
-        $restaurantController->match("/", array($this, 'showAll'))->bind('restaurant_list');
-        $restaurantController->get("/{name}", array($this, 'detail'))->bind('restaurant_detail');
-        $restaurantController->match("/{name}/reserve", [$this, "reserve"])->bind('restaurant_reserve');
-        $restaurantController->get("/{name}/reservations", array($this, 'detailReservations'))->bind('restaurant_detail_reservations');
-        /*$indexController->get("/show/{id}", array($this, 'show'))->bind('acme_show');
-        $indexController->match("/create", array($this, 'create'))->bind('acme_create');
-        $indexController->match("/update/{id}", array($this, 'update'))->bind('acme_update');
-        $indexController->get("/delete/{id}", array($this, 'delete'))->bind('acme_delete');*/
-        return $restaurantController;
+  /**
+   * Route settings
+   *
+   */
+  public function connect(Application $app) {
+    $restaurantController = $app['controllers_factory'];
+    $restaurantController->match("/", array($this, 'showAll'))->bind('restaurant_list');
+    $restaurantController->get("/{name}", array($this, 'detail'))->bind('restaurant_detail');
+    $restaurantController->match("/{name}/reserve", [$this, "reserve"])->bind('restaurant_reserve');
+    $restaurantController->get("/{name}/reservations", array($this, 'detailReservations'))->bind('restaurant_detail_reservations');
+    /*$indexController->get("/show/{id}", array($this, 'show'))->bind('acme_show');
+    $indexController->match("/create", array($this, 'create'))->bind('acme_create');
+    $indexController->match("/update/{id}", array($this, 'update'))->bind('acme_update');
+    $indexController->get("/delete/{id}", array($this, 'delete'))->bind('acme_delete');*/
+    return $restaurantController;
+  }
+
+  public function showAll(Application $app, Request $request) {
+    $em = $app['orm.ems']['grupo37'];
+    $form = $app['form.factory']->createBuilder(FormType::class, [])
+      ->add("name", TextType::class, [
+        'required' => false,
+      ])
+      ->add("city", ChoiceType::class, [
+        'choices' => array_map(function($item){ return $item["city"];}, $em->getRepository("Entity37\Restaurant")->getDistinctCities()),
+        'choice_label' => function($value, $key, $index) {
+            return $value;
+        },
+        "placeholder" => "Elige tu ciudad",
+        "required" => false
+      ])
+      ->add("rating", ChoiceType::class, [
+        'choices' => [0,1,2,3],
+        "placeholder" => "Minimum de estrellas",
+        "required" => false
+      ])
+      ->add("sort", ChoiceType::class, [
+        'choices' => ['Por ciudad' => 'ciudad', 'Alfabetico' => 'alfabetico', 'Por estrellas' => 'estrellas'],
+        'placeholder' => 'Ordenar los resultados...',
+        'required' => false
+      ])
+      ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted()){
+      $data = $form->getData();
+      $restaurants = $em->getRepository('Entity37\Restaurant')->getFiltered($data);
+
+    } else {
+      $restaurants = $em->getRepository('Entity37\Restaurant')->findAll();
     }
 
-    public function showAll(Application $app, Request $request) {
-        $em = $app['orm.ems']['grupo37'];
-        $form = $app['form.factory']->createBuilder(FormType::class, [])
-            ->add("name", TextType::class, [
-                'required' => false,
-            ])
-            ->add("city", ChoiceType::class, [
-                'choices' => array_map(function($item){ return $item["city"];}, $em->getRepository("Entity37\Restaurant")->getDistinctCities()),
-                'choice_label' => function($value, $key, $index) {
-                    return $value;
-                },
-                "placeholder" => "Elige tu ciudad",
-                "required" => false
-            ])
-            ->add("rating", ChoiceType::class, [
-                'choices' => [0,1,2,3],
-                "placeholder" => "Minimum de estrellas",
-                "required" => false
-            ])
-            ->add("sort", ChoiceType::class, [
-                'choices' => ['Por ciudad' => 'ciudad', 'Alfabetico' => 'alfabetico', 'Por estrellas' => 'estrellas'],
-                'placeholder' => 'Ordenar los resultados...',
-                'required' => false
-            ])
-            ->getForm();
+    return $app['twig']->render('restaurant/list.html.twig', [
+      'restaurants' => $restaurants,
+      'form' => $form->createView()
+    ]);
+  }
 
-        $form->handleRequest($request);
+  public function detail(Application $app, $name){
+    $em = $app['orm.ems']['grupo37'];
+    $restaurant = $em->getRepository('Entity37\Restaurant')->findOneByName($name);
+    if (!$restaurant){
+      $app['session']->getFlashBag()->add('error', 'Restaurante no encontrado');
+      return $app->redirect($app['url_generator']->generate('restaurant_list'));
+    }
+    $orderedReviews = $em->getRepository('Entity37\ReviewRestaurant')->getLatestReviewsFirst($restaurant);
+    return $app['twig']->render('restaurant/detail.html.twig', [
+      'restaurant' => $restaurant,
+      'reviews' => $orderedReviews
+    ]);
+  }
 
-        if ($form->isSubmitted()){
-            $data = $form->getData();
-            $restaurants = $em->getRepository('Entity37\Restaurant')->getFiltered($data);
-
-        } else {
-            $restaurants = $em->getRepository('Entity37\Restaurant')->findAll();
-        }
-
-        return $app['twig']->render('restaurant/list.html.twig', [
-            'restaurants' => $restaurants,
-            'form' => $form->createView()
-        ]);
+  public function reserve(Application $app, Request $request, $name){
+    $em = $app['orm.ems']['grupo37'];
+    $restaurant = $em->getRepository('Entity37\Restaurant')->findOneByName($name);
+    if (!$restaurant){
+      $app['session']->getFlashBag()->add('error', 'Restaurante no encontrado');
+      return $app->redirect($app['url_generator']->generate('restaurant_list'));
     }
 
-    public function detail(Application $app, $name){
-        $em = $app['orm.ems']['grupo37'];
-        $restaurant = $em->getRepository('Entity37\Restaurant')->findOneByName($name);
-        if (!$restaurant){
-            $app['session']->getFlashBag()->add('error', 'Restaurante no encontrado');
-            return $app->redirect($app['url_generator']->generate('restaurant_list'));
-        }
-        $orderedReviews = $em->getRepository('Entity37\ReviewRestaurant')->getLatestReviewsFirst($restaurant);
-	    return $app['twig']->render('restaurant/detail.html.twig', [
-            'restaurant' => $restaurant,
-            'reviews' => $orderedReviews
-        ]);
+    $form = $app['form.factory']->createBuilder(FormType::class, [])
+      // Client info
+      ->add("phoneNumber")
+      ->add("clientName")
+      // Reservation info
+      ->add("date", DateType::class,[
+          'widget' => 'single_text'
+      ])
+      ->add("time", TimeType::class,[
+          'widget' => 'choice',
+          'minutes' => ['00']
+      ])
+      ->add("quantity", IntegerType::class, [
+        'attr' => [
+          'min' => 0,
+          'max' => $restaurant->getCapacity()
+        ]
+      ])
+      ->add('submit', SubmitType::class)
+      ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted()){
+      $data = $form->getData();
+      $client = $em->getRepository('Entity37\Client')->findOneBy(['phone_number' => $data['phoneNumber']]);
+      if (!$client){
+        $client = new Client();
+        $client->setPhoneNumber($data['phoneNumber']);
+        $client->setName($data["clientName"]);
+      }
+
+      $reservation = new Reservation();
+      $reservation->setDate($data['date']);
+      $reservation->setTime($data['time']);
+      $reservation->setQuantity($data['quantity']);
+
+      $reservationRestaurant = new ReservationRestaurant();
+      $reservationRestaurant->setClient($client);
+      $reservationRestaurant->setRestaurant($restaurant);
+      $reservationRestaurant->setReservation($reservation);
+
+      $em->persist($client);
+      $em->persist($reservation);
+      $em->persist($reservationRestaurant);
+      $em->flush();
+
+      $app['session']->getFlashBag()->add('success', 'Reserva hecha exitosamente');
+      return $app->redirect($app['url_generator']->generate('restaurant_detail', ['name' => $name]));
     }
 
-    public function reserve(Application $app, Request $request, $name){
-        $em = $app['orm.ems']['grupo37'];
-        $restaurant = $em->getRepository('Entity37\Restaurant')->findOneByName($name);
-        if (!$restaurant){
-            $app['session']->getFlashBag()->add('error', 'Restaurante no encontrado');
-            return $app->redirect($app['url_generator']->generate('restaurant_list'));
-        }
+    return $app['twig']->render('restaurant/reserve.html.twig', [
+      'restaurant' => $restaurant,
+      'form' => $form->createView()
+    ]);
+  }
 
-        $form = $app['form.factory']->createBuilder(FormType::class, [])
-            // Client info
-            ->add("phoneNumber")
-            ->add("clientName")
-            // Reservation info
-            ->add("date", DateType::class,[
-                'widget' => 'single_text'
-            ])
-            ->add("time", TimeType::class,[
-                'widget' => 'choice',
-                'minutes' => ['00']
-            ])
-            ->add("quantity", IntegerType::class,[
-                'attr' => [
-                    'min' => 0,
-                    'max' => $restaurant->getCapacity()
-                ]
-            ])
-            ->add('submit', SubmitType::class)
-            ->getForm();
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted()){
-            $data = $form->getData();
-            $client = $em->getRepository('Entity37\Client')->findOneBy(['phone_number' => $data['phoneNumber']]);
-            if (!$client){
-                $client = new Client();
-                $client->setPhoneNumber($data['phoneNumber']);
-                $client->setName($data["clientName"]);
-            }
-
-            $reservation = new Reservation();
-            $reservation->setDate($data['date']);
-            $reservation->setTime($data['time']);
-            $reservation->setQuantity($data['quantity']);
-
-            $reservationRestaurant = new ReservationRestaurant();
-            $reservationRestaurant->setClient($client);
-            $reservationRestaurant->setRestaurant($restaurant);
-            $reservationRestaurant->setReservation($reservation);
-
-            $em->persist($client);
-            $em->persist($reservation);
-            $em->persist($reservationRestaurant);
-            $em->flush();
-
-            $app['session']->getFlashBag()->add('success', 'Reserva hecha exitosamente');
-            return $app->redirect($app['url_generator']->generate('restaurant_detail', ['name' => $name]));
-        }
-
-        return $app['twig']->render('restaurant/reserve.html.twig', [
-            'restaurant' => $restaurant,
-            'form' => $form->createView()
-        ]);
-
+  public function detailReservations(Application $app, $name){
+    $em = $app['orm.ems']['grupo37'];
+    $restaurant = $em->getRepository('Entity37\Restaurant')->findOneByName($name);
+    if (!$restaurant){
+      $app['session']->getFlashBag()->add('error', 'Restaurante no encontrado');
+      return $app->redirect($app['url_generator']->generate('restaurant_list'));
     }
-
-    public function detailReservations(Application $app, $name){
-        $em = $app['orm.ems']['grupo37'];
-        $restaurant = $em->getRepository('Entity37\Restaurant')->findOneByName($name);
-        if (!$restaurant){
-            $app['session']->getFlashBag()->add('error', 'Restaurante no encontrado');
-            return $app->redirect($app['url_generator']->generate('restaurant_list'));
-        }
-        return $app['twig']->render('restaurant/detail_reservations.html.twig', [
-            'restaurant' => $restaurant
-        ]);
-    }
+    return $app['twig']->render('restaurant/detail_reservations.html.twig', [
+      'restaurant' => $restaurant
+    ]);
+  }
 }
 
 
